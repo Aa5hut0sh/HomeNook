4e8ae7f52fa5e18f3c3dbfd9c2de2261699af6b7
Ashutosh sharma <aashutoshsharma2905@gmail.com>
2025-07-20 21:16:55 +0530

    Sample data on atlas

--- init/index.js
+++ init/index.js
@@ -1,15 +1,20 @@
+require('dotenv').config()
+if(process.env.NODE_ENV!="production" ){
+  require("dotenv").config();
+}
+
 const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/homenook";
+const dbUrl = process.env.ATLASDB_URL;
 
 main()
   .then(() => console.log("Connected to DB"))
   .catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect(MONGO_URL);
+  await mongoose.connect(dbUrl);
 }
 
 const initDB = async () => {
